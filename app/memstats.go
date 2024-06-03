a30d038587c0f48b693b999150bdabd97d40f91b
Steven Landers <steven.landers@gmail.com>
2024-06-03 14:36:29 -0400

    add release endpoint

--- app/memstats.go
+++ app/memstats.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 	"runtime"
+	"runtime/debug"
 )
 
 func getMemStats(w http.ResponseWriter, r *http.Request) {
@@ -14,8 +15,18 @@ func getMemStats(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(memStats)
 }
 
+func release(w http.ResponseWriter, r *http.Request) {
+	runtime.GC()
+	debug.FreeOSMemory()
+
+	// get memstats and release
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(`{"status": "ok"}`))
+}
+
 func startMemStats() {
 	http.HandleFunc("/memstats", getMemStats)
+	http.HandleFunc("/release", release)
 
 	// Start the HTTP server
 	go func() {
