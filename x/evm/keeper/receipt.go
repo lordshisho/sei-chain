b3b97388ed486a5c3fc9d6ce81d7e068a5964ff6
Steven Landers <steven.landers@gmail.com>
2024-06-03 17:30:18 -0400

    adjust receipt proto serialization

--- x/evm/keeper/receipt.go
+++ x/evm/keeper/receipt.go
@@ -6,6 +6,7 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/sei-protocol/sei-chain/x/evm/types"
+	"google.golang.org/protobuf/proto"
 )
 
 // Receipt is a data structure that stores EVM specific transaction metadata.
@@ -17,19 +18,19 @@ func (k *Keeper) GetReceipt(ctx sdk.Context, txHash common.Hash) (*types.Receipt
 	if bz == nil {
 		return nil, errors.New("not found")
 	}
-	r := types.Receipt{}
-	if err := r.Unmarshal(bz); err != nil {
+	r := &types.Receipt{}
+	if err := proto.Unmarshal(bz, r); err != nil {
 		return nil, err
 	}
-	return &r, nil
+	return r, nil
 }
 
 func (k *Keeper) SetReceipt(ctx sdk.Context, txHash common.Hash, receipt *types.Receipt) error {
-	//store := ctx.KVStore(k.storeKey)
-	//bz, err := receipt.Marshal()
-	//if err != nil {
-	//	return err
-	//}
-	//store.Set(types.ReceiptKey(txHash), bz)
+	store := ctx.KVStore(k.storeKey)
+	bz, err := proto.Marshal(receipt)
+	if err != nil {
+		return err
+	}
+	store.Set(types.ReceiptKey(txHash), bz)
 	return nil
 }
