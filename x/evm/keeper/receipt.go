25887312517b763b40c1c24810d8f731427559d0
Steven Landers <steven.landers@gmail.com>
2024-06-03 18:35:39 -0400

    Revert "attempt with buffer"
    This reverts commit 1e4ace17153d63a2c78c3a778ac6df5e27e460ce.

--- x/evm/keeper/receipt.go
+++ x/evm/keeper/receipt.go
@@ -2,7 +2,6 @@ package keeper
 
 import (
 	"errors"
-	"sync"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/ethereum/go-ethereum/common"
@@ -10,27 +9,8 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
-// Create a pool for byte slices
-var byteSlicePool = sync.Pool{
-	New: func() interface{} {
-		return make([]byte, 0, 1024) // 1KB initial capacity, adjust as needed
-	},
-}
-
-// getByteSlice retrieves a byte slice from the pool
-func getByteSlice() []byte {
-	return byteSlicePool.Get().([]byte)
-}
-
-// putByteSlice returns a byte slice to the pool
-func putByteSlice(b []byte) {
-	if cap(b) <= 1024 { // Only pool slices of 1KB or less, adjust as needed
-		byteSlicePool.Put(b[:0])
-	}
-}
-
 // Receipt is a data structure that stores EVM specific transaction metadata.
-// Many EVM applications (e.g. MetaMask) rely on being able to query receipt
+// Many EVM applications (e.g. MetaMask) relies on being on able to query receipt
 // by EVM transaction hash (not Sei transaction hash) to function properly.
 func (k *Keeper) GetReceipt(ctx sdk.Context, txHash common.Hash) (*types.Receipt, error) {
 	store := ctx.KVStore(k.storeKey)
@@ -47,18 +27,10 @@ func (k *Keeper) GetReceipt(ctx sdk.Context, txHash common.Hash) (*types.Receipt
 
 func (k *Keeper) SetReceipt(ctx sdk.Context, txHash common.Hash, receipt *types.Receipt) error {
 	store := ctx.KVStore(k.storeKey)
-
-	// Use the byte slice pool for marshaling
-	bz := getByteSlice()
-	defer putByteSlice(bz)
-
-	// Marshal the receipt
-	data, err := proto.Marshal(receipt)
+	bz, err := proto.Marshal(receipt)
 	if err != nil {
 		return err
 	}
-
-	// Set the data in the store
-	store.Set(types.ReceiptKey(txHash), data)
+	store.Set(types.ReceiptKey(txHash), bz)
 	return nil
 }
