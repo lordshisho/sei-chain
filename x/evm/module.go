4e6eda459bf6de85ed12bb6fda01831ba73e15ab
Philip Su <philip.su.522@gmail.com>
2024-07-24 11:43:20 -1000

    debug

--- x/evm/module.go
+++ x/evm/module.go
@@ -221,6 +221,11 @@ func (am AppModule) ExportGenesisStream(ctx sdk.Context, cdc codec.JSONCodec) <-
 			if i%10000 == 0 {
 				fmt.Printf("%d\n", i)
 			}
+			defer func() {
+				if r := recover(); r != nil {
+					fmt.Printf("x/evm genState that failed to Marshal = %+v\n", genState)
+				}
+			}()
 			gsJson, err := cdc.MarshalJSON(genState)
 			if err != nil {
 				fmt.Printf("x/evm genState that failed to Marshal = %+v\n", genState)
