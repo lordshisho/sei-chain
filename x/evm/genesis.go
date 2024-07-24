da0cae42394d1523981df4b09e052b65b93f4324
Jeremy Wei <jeremy.t.wei@gmail.com>
2024-07-24 16:32:38 -0400

    debug

--- x/evm/genesis.go
+++ x/evm/genesis.go
@@ -152,6 +152,7 @@ func ExportGenesisStream(ctx sdk.Context, k *keeper.Keeper) <-chan *types.Genesi
 			types.PointerCWCodePrefix,
 			types.PointerReverseRegistryPrefix,
 		} {
+			fmt.Println("current prefix = ", prefix)
 			var genesis types.GenesisState
 			genesis.Params = k.GetParams(ctx)
 			k.IterateAll(ctx, prefix, func(key, val []byte) bool {
@@ -161,7 +162,6 @@ func ExportGenesisStream(ctx sdk.Context, k *keeper.Keeper) <-chan *types.Genesi
 					Value:  val,
 				})
 				if len(genesis.Serialized) > GENESIS_EXPORT_STREAM_SERIALIZED_LEN_MAX {
-					fmt.Printf("Sending one prefix into ch, genesis = %+v\n", genesis)
 					ch <- &genesis
 					genesis = types.GenesisState{}
 					genesis.Params = k.GetParams(ctx)
